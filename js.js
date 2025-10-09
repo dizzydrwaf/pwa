6535a61d3b8b48b2987de06bf89b52287534f714
dizzy <dizzydrwaf@pm.me>
2025-10-09 11:08:57 +0200

    js linking in the service worker i think

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceworker.js').then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
+
+
 
 let deferredEvent;
 const installButton = document.getElementById('installbtn')
