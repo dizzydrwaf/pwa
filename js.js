8370894ef454b69213f3594d5be393b214033ff5
dizzy <dizzydrwaf@pm.me>
2025-10-09 10:20:31 +0200

    added js file and added content to manifest.json

---
+++ js.js
@@ -0,0 +1,18 @@
+
+let deferredEvent;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // prevent the browser from displaying the default install dialog
+  e.preventDefault();
+  
+  // Stash the event so it can be triggered later when the user clicks the button
+  deferredEvent = e;
+});
+
+installButton.addEventListener('click', () => {
+  // if the deferredEvent exists, call its prompt method to display the install dialog
+  if(deferredEvent) {
+    deferredEvent.prompt();
+  }
+});
+      
